76ec6eeac8fa5fd3f6f96e4ebbe977e75e96aef2
jdm79 <james.malvern@gmail.com>
2018-09-11 15:09:52 +0100

    fixes bootstrap button on login and register pages

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -25,17 +25,3 @@ feature 'login' do
   end
 
 end
-
-
-
-
-# feature 'login' do
-#   scenario 'enters name and email address' do
-#     visit '/'
-#     fill_in('email', with: 'james.malvern@gmail.com')
-#     fill_in('name', with: 'James')
-#     click_button 'Register'
-#     expect(page).to have_content('Welcome to MakersBnB, James!')
-#   end
-#
-# end
