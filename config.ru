4aa80bc0f706c866d425c5b6f109f1a37bd73a07
Freddie <fredjlawson@gmail.com>
2018-09-13 11:49:59 +0100

    adds new app.rb file

--- config.ru
+++ config.ru
@@ -1,3 +1,7 @@
 require_relative './app'
 
+require_relative 'app/controllers/properties_controller'
+# require_relative 'app/controllers/orders_controller'
+
+
 run Makersbnb
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,7 +17,8 @@
 
 ENV['RACK_ENV'] = 'test'
 
-require File.join(File.dirname(__FILE__), '..', 'app.rb')
+require File.join(File.dirname(__FILE__), '..', './app/controllers/properties_controller.rb')
+
 
 require 'capybara'
 require 'capybara/rspec'
