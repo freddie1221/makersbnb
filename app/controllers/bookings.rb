ad55cd2ff52c82ae6994d60c35a0324e50034ddc
Malachy Gilchrist <malachygilchrist@gmail.com>
2018-09-14 14:07:45 +0100

    bookings page displays property

--- app/controllers/bookings.rb
+++ app/controllers/bookings.rb
@@ -2,6 +2,9 @@
 class Bookings < Sinatra::Base
 
   get '/bookings/:id' do
+    @property = Property.find_by(id: params[:id].to_i)
+    p @property
+    erb :booking, :layout => :layout_logged_in
   end
 
   post '/bookings' do
