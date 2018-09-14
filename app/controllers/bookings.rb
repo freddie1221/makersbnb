4414da8a0e8661e81cce2f62508ef364f2d1db32
Malachy Gilchrist <malachygilchrist@gmail.com>
2018-09-14 16:18:35 +0100

    removes debugging lines

--- app/controllers/bookings.rb
+++ app/controllers/bookings.rb
@@ -3,15 +3,12 @@ class Bookings < Sinatra::Base
 
   get '/bookings/:id' do
     @property = Property.find_by(id: params[:id].to_i)
-    p @property
     erb :booking, :layout => :layout_logged_in
   end
 
   post '/bookings/:id' do
     booking = { account_id: session[:user_id], property_id: params[:id], date: params[:bookingdate], confirmed: false }
     Booking.create(booking)
-
-    p params
     redirect '/properties'
   end
 
